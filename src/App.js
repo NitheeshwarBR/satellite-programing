7cacf7fb7dbad5ae8bbc3e9e8473fb9b74a1b566
Pavan-B-N <pavangowda56005@gmail.com>
2022-09-30 19:32:03 +0530

    started with header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Header from "./components/header/Header"
 function App() {
   return (
     <>
-      <h1>Satellite</h1>
+      <Header/>
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css"
+function Header() {
+  return (
+    <>
+      <div className="app-header" >
+        <h2 className="app-title" >Satellite</h2>
+      </div>
+    </>
+  )
+}
+
+export default Header
