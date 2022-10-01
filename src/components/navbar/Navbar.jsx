7b05863bca2fedff6917bc6691243a2a06b40f07
Pratik K <undopatika@gmail.com>
2022-10-01 21:25:42 +0530

    Fonts and Favicon added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,29 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
-import NavbarLinks from "./NavbarLinks"
+import NavbarLinks from "./NavbarLinks";
 
 //icons
-import rocket from "./icons/rocket.svg"
-import mission from "./icons/mission.svg"
-import groups from "./icons/groups.svg"
-import info from "./icons/info.svg"
+import rocket from "./icons/rocket.svg";
+import mission from "./icons/mission.svg";
+import groups from "./icons/groups.svg";
+import info from "./icons/info.svg";
 function Navbar() {
   return (
     <>
       <div className="nav-container">
-            <NavbarLinks name="Vayuyastra" src={rocket}  href={"/"}  target={false} />
-            <NavbarLinks name="Mission" src={mission} />
-            <NavbarLinks name="Teams" src={groups} />
-            <NavbarLinks name="About" src={info} />
-
+        <div>
+          <NavbarLinks name="Vayuyastra" src={rocket} />
+          <NavbarLinks name="Mission" src={mission} />
+          <NavbarLinks name="Teams" src={groups} />
+          <NavbarLinks name="About" src={info} />
+        </div>
+        <div className="nav-bottom-container">
+        
+        <a href="https://www.orionprotocol.io/orn" className="plain-link primary">Feedback</a>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/navbar/NavbarLinks.jsx
+++ src/components/navbar/NavbarLinks.jsx
@@ -1,18 +1,16 @@
-
 function NavbarLinks(props) {
-
   return (
     <>
-      <div className="navbarlink-container" >
-        <a href={props.href} target={props.target ?  "_blank" : "_self"} >
-        <div  className="navbar-link-icon">
-            <img src={props.src} alt={props.name}  className="navbar-svg-icon" />
-        </div>
+      <div className="navbarlink-container">
+        <a href={props.href}>
+          <div className="navbar-link-icon">
+            <img src={props.src} alt={props.name} className="navbar-svg-icon" />
+          </div>
         </a>
-        <pre  className="navlink-title" >{props.name}</pre>
+        <p className="navlink-title">{props.name}</p>
       </div>
     </>
-  )
+  );
 }
 
-export default NavbarLinks
+export default NavbarLinks;
